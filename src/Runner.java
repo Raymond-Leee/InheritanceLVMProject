def3d9045431f26ad2d778e81b8815a7e8664b0c
BT_1E10_12 <sample@bths.edu>
2022-03-21 12:30:16 -0400

    Commit 3/21/22

--- src/Runner.java
+++ src/Runner.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
         Scanner user = new Scanner(System.in);
-
+        boolean exit = false;
+        System.out.println("Welcome to the LVM system! Enter your commands:");
+        while (!exit)
+        {
+            System.out.print("cmd# ");
+            String cmd = user.next();
+            if (cmd.equalsIgnoreCase("exit"))
+            {
+                System.out.println("Saving data. Goodbye!");
+                exit = true;
+            }
+        }
     }
 }
